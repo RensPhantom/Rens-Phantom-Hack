bac137a3a6c989570a26c21507a0af1b406bf6da
RensPhantom <115673016+RensPhantom@users.noreply.github.com>
2022-10-13 01:52:45 +0200

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -3,7 +3,7 @@ local script_details = {
     version = "1.0.0",
 }
 
-local url = script_details.debug and "http://localhost:6845" or "https://raw.githubusercontent.com/Averiias/purple-haze-pf/main"
+local url = script_details.debug and "http://localhost:6845" or "https://raw.githubusercontent.com/RensPhantom/Rens-Phantom-Hack/main"
 
 local out = script_details.debug and function(T, ...)
     return warn("[rens phantom hack - debug]: "..T:format(...))
